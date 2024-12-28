8d834ca8891b15c36e7a8903f3ef55b7c4c9bf3b
Abel <abeladdis3@gmail.com>
2024-12-28 15:20:54 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Typing Animation */
+var typed = new Typed(".typing", {
+    strings:["","House Renovating", "Cleaning Services","Storage Hauling",""],
+    typeSpeed: 50,
+    BackSpeed: 10,
+    loop:true
+})
